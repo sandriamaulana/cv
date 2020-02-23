3595720a3baf3cd617da4c6ed9f7c57bc1199886
sandria <sandriamaulana31@gmail.com>
2020-02-23 17:33:25 +0700

    init

---
+++ js/effect.js
@@ -0,0 +1,8 @@
+window.onscroll = function () { scrollFunction() };
+// shrink when scroll
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        $('.data-diri').addClass('slide-effect');
+
+    }
+}
